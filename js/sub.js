bb5ed1dd1be71aba93d4c72d6088dd11ba97bf33
2skyyy <128768622+2skyyy@users.noreply.github.com>
2023-04-21 21:35:35 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -8,7 +8,27 @@ $(document).on('ready', function () {
         infinite: true,
         centerMode: true,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 1,
+        responsive: [ // 반응형 웹 구현 옵션
+        {  
+            breakpoint: 1800,
+            settings: {
+                slidesToShow:3
+            } 
+        },
+        { 
+            breakpoint: 1300,
+            settings: {	
+                slidesToShow:2 
+            } 
+        },
+        { 
+            breakpoint: 930,
+            settings: {	
+                slidesToShow:1
+            } 
+        }
+    ]
     });
 
     $(".regular").slick({
